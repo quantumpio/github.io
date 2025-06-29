30c7a897d1277e4d2129fbc893068ae19f8375c1
Persofikcja <dezoksyrybonukleinowy33@gmail.com>
2025-06-29 21:01:50 +0200

    portfolio upload

---
+++ Piotr Sujecki — QA Engineer_files/hoisted.BGvJLjt_.js
@@ -0,0 +1,1 @@
+(function(e,n,r,t,m){e[t]=e[t]||[],e[t].push({"gtm.start":new Date().getTime(),event:"gtm.js"});var g=n.getElementsByTagName(r)[0],a=n.createElement(r),s="";a.async=!0,a.src="https://www.googletagmanager.com/gtm.js?id="+m+s,g.parentNode.insertBefore(a,g)})(window,document,"script","dataLayer","GTM-WJBMS2B");
